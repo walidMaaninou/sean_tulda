c2a389a2e229537d19fa0e92491654f6d9692c5d
walidMaaninou <83701453+walidMaaninou@users.noreply.github.com>
2025-03-14 11:53:38 +0000

    versioning problem

--- TulsaCountyScraper.py
+++ TulsaCountyScraper.py
@@ -68,7 +68,7 @@ if initialize_button:
         chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
 
         # Use Service to manage the WebDriver
-        service = Service(ChromeDriverManager(version="120.0.6099.224").install())
+        service = Service(ChromeDriverManager(driver_version="120.0.6099.224").install())
 
         # Correct WebDriver initialization
         driver = webdriver.Chrome(service=service, options=chrome_options)
